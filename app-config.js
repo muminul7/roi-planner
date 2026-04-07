5f5627227ef046ea355d3a2aaeb4376c85eb124b
Muminul islam <92967685+muminul7@users.noreply.github.com>
2026-04-07 12:03:31 +0600

    Fix Supabase project URL

--- app-config.js
+++ app-config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  supabaseUrl: 'https://xzsojeqvylfyyuwtjmom.supabase.co',
+  supabaseUrl: 'https://xzsojeqyvlfyyuwtjmom.supabase.co',
   supabaseAnonKey: 'sb_publishable_56mvni4JFO4Dl-Fv_biZ-w_8Kkk82eI'
 };
