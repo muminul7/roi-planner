beca2133f2f0029bb8a50022431508c486cba248
Muminul islam <92967685+muminul7@users.noreply.github.com>
2026-04-07 11:51:54 +0600

    Configure Supabase client settings

--- app-config.js
+++ app-config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  supabaseUrl: 'https://xzsojeqvylfyyuwtjmom.supabase.co',
+  supabaseAnonKey: 'sb_publishable_56mvni4JFO4Dl-Fv_biZ-w_8Kkk82eI'
 };
